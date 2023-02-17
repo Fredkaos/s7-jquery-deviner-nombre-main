57812b150317e4fb896af6ed1c535a563f28b697
1079058 <1079058@cegepoutaouais.qc.ca>
2023-02-17 14:56:04 -0500

    Devoir

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-let aleatoire = Math.ceil(Math.random()*100); //le nombre à deviner
+$(document).ready(function (){
+    // Le nombre à deviner
+    aleatoire = Math.ceil(Math.random()*100);
+    $("button").click(function (){
+        $choix = $("#essai").val();
+        if (aleatoire >= $choix){
+            for (x=1; x <= $choix;x++){
+                $("#"+x).remove();
+            }
+        }
+        else if (aleatoire <= $choix){
+            for (x=$choix; x <= 100 ;x++){
+                $("#"+x).remove();
+            }
+        }
+    });
+});
