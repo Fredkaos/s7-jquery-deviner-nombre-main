e607e4e9253ed9e3f3e2a982c57355e09a88886c
Fred <1079058@cegepoutaouais.qc.ca>
2023-02-20 20:22:32 -0500

    Jquery2

--- script.js
+++ script.js
@@ -3,15 +3,24 @@ $(document).ready(function (){
     aleatoire = Math.ceil(Math.random()*100);
     $("button").click(function (){
         $choix = $("#essai").val();
-        if (aleatoire >= $choix){
-            for (x=1; x <= $choix;x++){
+        if (aleatoire > $choix){
+            for (x=1; x <= $choix; x++) {
+                $("#" + x).remove();
+            }
+        }
+        else if (aleatoire < $choix){
+            for (x=$choix; x <= 100; x++){
                 $("#"+x).remove();
             }
         }
-        else if (aleatoire <= $choix){
-            for (x=$choix; x <= 100 ;x++){
+        else {
+            $("#validation").append("<p>Bravo!</p><br>" + "<button id='rejouer'>Reessayer</button>");
+            for (x=1; x <= 100; x++){
                 $("#"+x).remove();
             }
+            $("#rejouer").click(function (){
+                location.reload(true);
+            });
         }
     });
 });
